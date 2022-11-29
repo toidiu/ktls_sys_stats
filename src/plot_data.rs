d26fccea448d6142894d532eaf9899bf95852984
toidiu <apoorv@toidiu.com>
2022-11-29 00:35:32 -0800

    color

--- src/plot_data.rs
+++ src/plot_data.rs
@@ -1,5 +1,8 @@
 use crate::parse_data::{SysGroup, SysStats};
-use plotly::{layout::GridPattern, layout::Layout, layout::LayoutGrid, Plot, Scatter};
+use plotly::{
+    color::Rgb, common::Marker, layout::GridPattern, layout::Layout, layout::LayoutGrid, Plot,
+    Scatter,
+};
 use regex::Regex;
 
 pub fn plot_stats<F, T>(sys_group: SysGroup, f: F, title_units: &str, show: bool)
@@ -9,7 +12,6 @@ where
 {
     let mut plot = Plot::new();
 
-    let mut subplot = 1;
     for stat in sys_group.sys.into_iter() {
         let name = stat.title.clone();
         let trace = Scatter::new(stat.sec.clone(), f(stat))
@@ -17,7 +19,6 @@ where
             .x_axis("x")
             .y_axis("y");
         plot.add_trace(trace);
-        subplot += 1;
     }
 
     let layout = Layout::new()
@@ -50,7 +51,7 @@ where
 
     let mut subplot = 1;
     for sys_group in sys_groups {
-        for stat in sys_group.sys.into_iter() {
+        for (rotate_color, stat) in sys_group.sys.into_iter().enumerate() {
             let subplot_name = if subplot == 1 {
                 "".to_string()
             } else {
@@ -59,15 +60,23 @@ where
             let name = stat.title.clone();
             let re = Regex::new(r"\d*[k,b]$").unwrap();
             let legend_group = re.replace(&name, "");
-            println!("{}", legend_group);
+            // println!("{}", legend_group);
+
+            let color = match rotate_color % 3 {
+                0 => Rgb::new(44, 160, 44),
+                1 => Rgb::new(31, 119, 180),
+                _ => Rgb::new(255, 127, 14),
+            };
 
             let trace = Scatter::new(stat.sec.clone(), f(stat))
                 .name(name.clone())
                 .legend_group(legend_group)
+                .marker(Marker::new().color(color))
                 .x_axis(format!("x{}", subplot_name))
                 .y_axis(format!("y{}", subplot_name));
             plot.add_trace(trace);
         }
+
         subplot += 1;
     }
 
